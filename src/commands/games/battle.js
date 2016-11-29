978dee83dd3230bc75b50b156e33c39e6e160480
pyraxo <pyraxo@tfwno.gf>
2016-11-30 00:51:03 +0800

    :bug: Member status in battle.js

--- src/commands/games/battle.js
+++ src/commands/games/battle.js
@@ -30,7 +30,7 @@ class Battle extends Command {
       return this[rawArgs[0]](container, responder, companions)
     }
 
-    if (args.action[0].member.status !== 'online') {
+    if (args.action[0].status !== 'online') {
       return responder.error('{{errors.notOnline}}')
     }
 
