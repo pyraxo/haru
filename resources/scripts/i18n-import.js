37cb045c6eda30e19fda01e0503aeca6b7c0c217
pyraxo <pyraxo@tfwno.gf>
2016-12-12 23:06:36 +0800

    :globe_with_meridians: Scripts for locale compiling

---
+++ resources/scripts/i18n-import.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+
+const FOLDERPATH = path.join(__dirname, '..', 'i18n')
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'strings.json')))
+for (const lang in data) {
+	if (lang === 'en') continue
+	const LANGPATH = path.join(__dirname, '..', 'i18n', lang)
+	for (const key in data[lang]) {
+		const FILEPATH = path.join(LANGPATH, key + '.json')
+		fs.writeFileSync(FILEPATH, data[lang][key], null, 2)
+		console.log('wrote to ' + FILEPATH)
+	}
+}
+
+console.log('finished import')
