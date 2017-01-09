a87948432f7d93e629e4b0b02f31ac60ddf43827
pyraxo <pyraxo@tfwno.gf>
2017-01-09 14:11:57 +0800

    :bug: Fix middleware instantiating new Guild entry

--- src/middleware/verifySettings.js
+++ src/middleware/verifySettings.js
@@ -4,14 +4,11 @@ module.exports = {
     const { client, msg, isPrivate, data, db } = container
     try {
       if (isPrivate) {
-        let channel = await client.getDMChannel(msg.author.id)
+        const channel = await client.getDMChannel(msg.author.id)
         container.settings = new db.Guild({ id: channel.id })
         return container
       }
-      let settings = await data.Guild.fetch(msg.guild.id)
-      if (!settings) {
-        settings = new db.Guild({ id: msg.guild.id })
-      }
+      const settings = await data.Guild.fetch(msg.guild.id)
       settings.deleted = false
       await settings.save()
       container.settings = settings
