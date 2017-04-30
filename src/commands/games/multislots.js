7959cb9a7b95c9dc6b202cb08fcfd7dd88fc870c
washingdone <yu.gi.ohlover1984@gmail.com>
2017-04-30 13:36:31 -0500

    Adds multi-line slots command

---
+++ src/commands/games/multislots.js
@@ -0,0 +1,140 @@
+const moment = require('moment')
+const { Command } = require('../../core')
+
+class Multislots extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'multislots',
+      description: 'Multi-line slot machine command',
+      usage: [{ name: 'bet', type: 'int', optional: true, default: 1 }],
+      aliases: ['multislot'],
+      cooldown: 6
+    })
+
+    this.reel = [
+      '🍇', '🍊', '🇱🇻', '🍈', '🍌', '🍒', '🍉', '🔔', '💎', '🍐', '🍇', '🍊',
+      '🍈', '🍒', '🍌', '🍉', '🇱🇻', '💎', '🍌', '🔔', '🍇', '🍐', '🍊',
+      '🍊', '🍌', '🍒', '🇱🇻', '🍐', '🍈', '🍇', '🍌'
+    ]
+
+    this.wins = {
+      '🍒 x 1': 1,
+      '🍒 x 2': 2,
+      '🍒 x 3': 5,
+      '7⃣ x 2': 25,
+      '7⃣ x 3': 75,
+      '🍐 x 3': 10,
+      '🍈 x 3': 10,
+      '🍇 x 3': 10,
+      '🍊 x 3': 10,
+      '🍌 x 3': 10,
+      '💎 x 2': 15,
+      '💎 x 3': 75,
+      '🔔 x 3': 25,
+      '🍉 x 3': 10,
+      '🇱🇻 x 2': 20,
+      '🇱🇻 x 3': 50
+    }
+  }
+
+  get generateSlots () {
+    const reels = this.reel
+    let machine = []
+    for (let i = 0; i < 3; i++) {
+      const res = ~~(Math.random() * (reels.length - 2))
+      machine.push(reels.slice(res, res + 3))
+    }
+    return machine
+  }
+
+  checkWinnings (payline1, payline2, payline3, amt) {
+    let wins = []
+    const res = payline1.reduce((p, c) => {
+      p[c] = (p[c] || 0) + 1
+      return p
+    }, {})
+    for (const r in res) {
+      const v = `${r} x ${res[r]}`
+      if (this.wins[v]) wins.push([v, this.wins[v] * amt])
+    }
+    const res2 = payline2.reduce((p2, c2) => {
+      p2[c2] = (p2[c2] || 0) + 1
+      return p2
+    }, {})
+    for (const r2 in res2) {
+      const v2 = `${r2} x ${res2[r2]}`
+      if (this.wins[v2]) wins.push([v2, this.wins[v2] * amt])
+    }
+    const res3 = payline3.reduce((p3, c3) => {
+      p3[c3] = (p3[c3] || 0) + 1
+      return p3
+    }, {})
+    for (const r3 in res3) {
+      const v3 = `${r3} x ${res3[r3]}`
+      if (this.wins[v3]) wins.push([v3, this.wins[v3] * amt])
+    }
+    return wins
+  }
+
+  doSlots (bet, lowerChances = false) {
+    const machine = this.generateSlots
+    const payline1 = [machine[0][0], machine[1][0], machine[2][0]]
+    const payline2 = [machine[0][1], machine[1][1], machine[2][1]]
+    const payline3 = [machine[0][2], machine[1][2], machine[2][2]]
+    const winnings = this.checkWinnings(payline1, payline2, payline3, bet)
+
+    const reroll = Math.random() >= 0.15
+    return winnings.length > 0 && lowerChances && reroll
+    ? this.doSlots(bet, lowerChances) : [ machine, payline1, payline2, payline3, winnings ]
+  }
+
+  async handle ({ msg, args, data, settings, cache }, responder) {
+    let dailyWins = await cache.client.getAsync(`slots:${msg.author.id}`)
+    if (parseInt(dailyWins, 10) >= 1000000) {
+      const res = await cache.client.pttlAsync(`slots:${msg.author.id}`)
+      return responder.error('{{dailyLimit}}', {
+        time: `${moment(res + moment()).fromNow(true)}`
+      })
+    }
+    const user = await data.User.fetch(msg.author.id)
+    if (args.bet > 10000) args.bet = 10000
+    if (args.bet < 1) return responder.error('{{yudodis}}')
+    if (user.credits < args.bet) {
+      return responder.error('{{insufficient}}', {
+        amount: `**${args.bet - user.credits}**`,
+        command: `**\`${settings.prefix}wage\`**`
+      })
+    }
+
+    const [machine, payline1, payline2, payline3, winnings] = this.doSlots(args.bet, args.bet > 1000)
+    try {
+      user.credits -= args.bet
+      let total = 0
+      for (const win of winnings) {
+        user.credits += win[1]
+        total += win[1]
+      }
+      await user.save()
+      await cache.client.multi()
+      .incrby(`slots:${msg.author.id}`, total)
+      .expire(`slots:${msg.author.id}`, 86400)
+      .execAsync()
+    } catch (err) {
+      return responder.error()
+    }
+    return responder.send([
+      '**__   S   L   O   T   S   __**',
+      `> ${payline1.join(' ')} <`,
+      `> ${payline2.join(' ')} <`,
+      `> ${payline3.join(' ')} <`,
+      winnings.length
+      ? `{{won}}\n\n${winnings.map(w => `${w[0]}: **${w[1]} {{credits}}**`).join('\n')}`
+      : '{{lost}}'
+    ], {
+      user: `**${msg.author.username}**`,
+      amount: `**${args.bet}**`
+    })
+  }
+}
+
+module.exports = Multislots
