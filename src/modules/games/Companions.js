2e0da153890d53bc3eef4d1d6044a3dd7b8d90b8
ærion <pyraxo@tfwno.gf>
2017-05-02 23:51:53 +0800

    :zap: Winner user write should occur only once

--- src/modules/games/Companions.js
+++ src/modules/games/Companions.js
@@ -247,7 +247,6 @@ class Companions extends Module {
     winUser.credits += battle.fee * 2
     winUser.companion.xp = (winUser.companion.xp || 0) + ~~(Math.random() * 5) + 2
     winUser.companion.stats.wins += 1
-    await winUser.saveAll({ companion: true })
 
     const loseUser = await this.db.User.fetch(battle[loser], { companion: true })
     loseUser.companion.xp = (loseUser.companion.xp || 0) + ~~(Math.random() * 3) + 1
@@ -277,8 +276,8 @@ class Companions extends Module {
       user.credits -= bet
       await user.save()
       winUser.credits += Math.ceil(bet / 2)
-      await winUser.save()
     }
+    await winUser.saveAll({ companion: true })
 
     if (winners.length || losers.length) {
       await this.send(battle.channel, [
