84d303ef0823c59bb4a2e9dcd4189e5a11bf8fbe
Mark <yu.gi.ohlover1984@gmail.com>
2018-02-10 15:53:12 -0600

    :bug: fixes @bot prefix for real this time

--- src/middleware/parseMessage.js
+++ src/middleware/parseMessage.js
@@ -1,9 +1,8 @@
 module.exports = {
   priority: 10,
   process: container => {
-    const { settings, msg, commands, _overwrite } = container
+    const { settings, msg, commands, client, _overwrite } = container
     const { prefix } = settings
-<<<<<<< HEAD
     const defPrefix = `<@${client.user.id}>`
 
     if (!_overwrite) {
@@ -14,11 +13,6 @@ module.exports = {
         newfix = defPrefix + ' '
       }
       const rawArgs = msg.content.substring(newfix.length).split(' ')
-=======
-
-    if (!_overwrite) {
-      const rawArgs = msg.content.substring(prefix.length).split(' ')
->>>>>>> 609b83dc3bb91d5dc754f84a122710ed06a7a8ae
       container.trigger = rawArgs[0].toLowerCase()
       container.isCommand = commands.has(container.trigger)
       container.rawArgs = rawArgs.slice(1).filter(v => !!v)
