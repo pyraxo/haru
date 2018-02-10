609b83dc3bb91d5dc754f84a122710ed06a7a8ae
Mark <yu.gi.ohlover1984@gmail.com>
2018-02-10 21:53:15 +0100

    :rewind: Revert @bot prefix

--- src/middleware/parseMessage.js
+++ src/middleware/parseMessage.js
@@ -1,13 +1,11 @@
 module.exports = {
   priority: 10,
   process: container => {
-    const { settings, msg, commands, client, _overwrite } = container
+    const { settings, msg, commands, _overwrite } = container
     const { prefix } = settings
-    const defPrefix = `<@${client.user.id}> `
 
     if (!_overwrite) {
-      const chk = msg.content.startsWith(prefix)
-      const rawArgs = msg.content.substring((chk ? prefix : defPrefix).length).split(' ')
+      const rawArgs = msg.content.substring(prefix.length).split(' ')
       container.trigger = rawArgs[0].toLowerCase()
       container.isCommand = commands.has(container.trigger)
       container.rawArgs = rawArgs.slice(1).filter(v => !!v)
